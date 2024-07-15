b128afb7c4fa19bf806d3a5e6e6ba86d79abbd46
Aligehri <133458600+Aligehri@users.noreply.github.com>
2024-07-15 15:53:45 -0600

    Update procedure.sql
    Query en base de datos DESASTRES

--- redshift/procesos/procedure.sql
+++ redshift/procesos/procedure.sql
@@ -5,13 +5,34 @@ DROP PROCEDURE IF EXISTS pETL_Desastres;
 CREATE OR REPLACE PROCEDURE pETL_Desastres () LANGUAGE plpgsql
 AS $$
   BEGIN
-  DROP TABLE
-  END;
-  BEGIN
-    INSERT INTO DESASTRES_FINAL (Cuatrenio, Temp_AVG, Oxi_AVG, T_Tsunamis, T_OlasCalor, T_Terremotos, T_Erupciones, T_Incendios, M_Jovenes_AVG FLOAT, M_Adutos_AVG, M_Ancianos_AVG)
-    SELECT
-    CASE
-      WHEN 
-    END,
+  CREATE TABLE auxiliar AS
+  SELECT 
+  año, Temperatura, Oxigeno,
+  Tsunamis, Olas_Calor, Terremotos, Erupciones,
+  R_Menor15, R_15_a_30, R_30_a_45, R_45_a_60, R_M_a_60
+  FROM clima
+  JOIN desastres ON desastres.año = clima.año
+  JOIN muertes ON muertes.año = clima.año
+  
+  CREATE TABLE auxiliar1 AS
+  SELECT 
+  CASE
+    WHEN 2023<= año <= 2026 THEN '2023 - 2026'
+    ELSE '2027 - 2030'
+  END AS Cuatrienio,
+  AVG(Temperatura) AS Temp_AVG,
+  AVG(Oxigeno) AS Oxi_AVG,
+  SUM(Tsunamis) AS T_Tsunamis,
+  SUM(Olas_Calor) AS T_OlasCalor,
+  SUM(Terremotos) AS T_Terremotos,
+  SUM(Erupciones) AS T_Erupciones,
+  SUM(R_Menor15) + SUM(R_15_a_30) AS Jovenes,
+  SUM(R_30_a_45) + SUM(R_45_a_60) AS Adultos, 
+  SUM(R_M_a_60) AS Ancianos
+  FROM auxiliar
+  GROUP BY  Cuatrienio;
+  
+  SELECT *
+    FROM auxiliar1;
   END;
 $$;
