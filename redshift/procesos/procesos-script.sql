0b60b3ade0ff538c4a4742d79bef4a3c6f069422
Aligehri <133458600+Aligehri@users.noreply.github.com>
2024-07-15 16:44:05 -0600

    Update procesos-script.sql

--- redshift/procesos/procesos-script.sql
+++ redshift/procesos/procesos-script.sql
@@ -1,16 +1,12 @@
--- 1.crear base de datos
 DROP DATABASE IF EXISTS DESASTRES;
 CREATE DATABASE DESASTRES;
 \c DESASTRES ;
---
 
--- 2. crear tabla clima futuro global
 CREATE TABLE clima
 (año INT NOT NULL PRIMARY KEY,
 Temperatura FLOAT NOT NULL,
 Oxigeno FLOAT NOT NULL);
 
--- Insertar valores manualmente
 INSERT INTO clima VALUES (2023, 22.5,230);
 INSERT INTO clima VALUES (2024, 22.7,228.6);
 INSERT INTO clima VALUES (2025, 22.9,227.5);
@@ -20,7 +16,6 @@ INSERT INTO clima VALUES (2028, 23.4,226.2);
 INSERT INTO clima VALUES (2029, 23.6,226.1);
 INSERT INTO clima VALUES (2030, 23.8,225.1);
 
--- 3. crear tabla desastres proyectados globales
 CREATE TABLE desastres
 (año INT NOT NULL PRIMARY KEY,
 Tsunamis INT NOT NULL,
@@ -29,7 +24,6 @@ Terremotos INT NOT NULL,
 Erupciones INT NOT NULL,
 Incendios INT NOT NULL);
 
--- Insertar valores manualmente
 INSERT INTO desastres VALUES (2023, 2,15, 6,7,50);
 INSERT INTO desastres VALUES (2024, 1,12, 8,9,46);
 INSERT INTO desastres VALUES (2025, 3,16, 5,6,47);
@@ -39,7 +33,6 @@ INSERT INTO desastres VALUES (2028, 4,18, 3,2,39);
 INSERT INTO desastres VALUES (2029, 2,19, 5,6,49);
 INSERT INTO desastres VALUES (2030, 4,20, 6,7,50);
 
--- 4. crear tabla muertes proyectadas por rangos de edad
 CREATE TABLE muertes
 (año INT NOT NULL PRIMARY KEY,
 R_Menor15 INT NOT NULL,
@@ -48,7 +41,6 @@ R_30_a_45 INT NOT NULL,
 R_45_a_60 INT NOT NULL,
 R_M_a_60 INT NOT NULL);
 
--- Insertar valores manualmente
 INSERT INTO muertes VALUES (2023, 1000,1300, 1200,1150,1500);
 INSERT INTO muertes VALUES (2024, 1200,1250, 1260,1678,1940);
 INSERT INTO muertes VALUES (2025, 987,1130, 1160,1245,1200);
@@ -58,7 +50,6 @@ INSERT INTO muertes VALUES (2028, 957,987, 1856,1567,1756);
 INSERT INTO muertes VALUES (2029, 1285,1376, 1465,1432,1236);
 INSERT INTO muertes VALUES (2030, 1145,1456, 1345,1654,1877);
 
--- 5. Crear base de datos para alojar resumenes de estadisticas
 DROP DATABASE IF EXISTS DESASTRES_BDE;
 CREATE DATABASE DESASTRES_BDE;
 \c DESASTRES_BDE;
