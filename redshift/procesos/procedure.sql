34a710d3028577682237493d139d7655c03d4d55
Aligehri <133458600+Aligehri@users.noreply.github.com>
2024-07-15 14:53:11 -0600

    Update procedure.sql
    AVANCE

--- redshift/procesos/procedure.sql
+++ redshift/procesos/procedure.sql
@@ -1,1 +1,17 @@
-\c DESASTRES_BDE;
+\c DESASTRES;
+
+DROP PROCEDURE IF EXISTS pETL_Desastres;
+
+CREATE OR REPLACE PROCEDURE pETL_Desastres () LANGUAGE plpgsql
+AS $$
+  BEGIN
+  DROP TABLE
+  END;
+  BEGIN
+    INSERT INTO DESASTRES_FINAL (Cuatrenio, Temp_AVG, Oxi_AVG, T_Tsunamis, T_OlasCalor, T_Terremotos, T_Erupciones, T_Incendios, M_Jovenes_AVG FLOAT, M_Adutos_AVG, M_Ancianos_AVG)
+    SELECT
+    CASE
+      WHEN 
+    END,
+  END;
+$$;
